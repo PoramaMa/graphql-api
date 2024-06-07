15f796c33f02bdcccf6ad0eb910dc4a6ba61606d
Porama Arsama <porama.prm@gmail.com>
2024-06-07 14:44:37 +0700

    Update User.ts

--- src/graphql/models/User.ts
+++ src/graphql/models/User.ts
@@ -1,17 +1,23 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { UserSetting } from './UserSetting';
 
+@Entity({ name: 'users' })
 @ObjectType()
 export class User {
+  @PrimaryGeneratedColumn()
   @Field((type) => Int)
   id: number;
 
+  @Column()
   @Field()
   username: string;
 
+  @Column()
   @Field({ nullable: true })
   displayName?: string;
 
+  @Column()
   @Field({ nullable: true })
   settings?: UserSetting;
 }
