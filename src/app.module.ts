b9ead022396796b6761cfb31e8635069a3940990
Porama Arsama <porama.prm@gmail.com>
2024-06-07 13:59:06 +0700

    up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserResolver } from './graphql/resolvers/UserResolver';
 import { UserSettingsResolver } from './graphql/resolvers/UserSettingsResolver';
+require('dotenv').config();
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
     }),
   ],
   controllers: [],
